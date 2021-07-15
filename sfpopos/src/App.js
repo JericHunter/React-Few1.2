25ab000e7b073a7805c4691cea91e1e5f99d29b6
JericHunter <51177847+JericHunter@users.noreply.github.com>
2021-07-15 19:56:05 +0000

    dynamic conteent and footer

--- sfpopos/src/App.js
+++ sfpopos/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Title from './components/Title';
+import Footer from './components/Footer';
 import POPOSList from './components/POPOSList';
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
     <div className="App">
         <Title />
         <POPOSList />
+        <Footer />
     </div>
   );
 }
-
 export default App;
---
+++ sfpopos/src/components/POPOSSpace.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/POPOSSpace.css';
+
+function POPOSSpace(props) {
+    const {name, image, address} = props;
+    return (
+        <div className="POPOSSpace">
+            <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+            <h1>{name}</h1>
+            <div className="address">{address}</div>
+        </div>
+    );
+}
